3a12aa8c6690c3c3b532f51d16b5158f1f77b119
Victor Queiroz <victorcqueirozg@gmail.com>
2018-11-30 19:36:05 -0300

    create include/av.h file

---
+++ include/av.h
@@ -0,0 +1,12 @@
+#ifndef FFMPEG_CPP_AV_H
+#define FFMPEG_CPP_AV_H
+
+#include "../src/av.h"
+#include "../src/Codec.h"
+#include "../src/Format.h"
+#include "../src/Frame.h"
+#include "../src/Packet.h"
+#include "../src/Parser.h"
+#include "../src/SwsContext.h"
+
+#endif //FFMPEG_CPP_AV_H
